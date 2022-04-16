b1d584f2d9e7fe2eb46eba70dc80d6c2d6043bb4
sanghavp <hahuusang2khl@gmail.com>
2022-04-16 21:23:26 +0700

    add html

--- dantri.js
+++ dantri.js
@@ -4,10 +4,11 @@ const cheerio = require('cheerio');
 (async () => {
   const browser = await puppeteer.launch({headless: false, devtools: true})
   const page = await browser.newPage()
+  // Open website
   await page.goto('https://www.tiktok.com/tag/onhiemmoitruong')
   //https://github.com/cheeriojs/cheerio
 
-  
+  // Scroll to end page
   let previousHeight;
   while (true) {
     try {
@@ -19,19 +20,21 @@ const cheerio = require('cheerio');
       break
     }
   }
-  // console.log(info.length);
+
+  // full Content in website
   let content = await page.content();
   var $ = cheerio.load(content);
+  // Div tag contains title
   var info = $(content).find("div.tiktok-1okfv2l-DivTagCardDesc-StyledDivTagCardDescV2")
-  // console.log("info",info);
+  // Div tag contain other
+  var other = $(content).find("div.tiktok-x6f6za-DivContainer-StyledDivContainerV2")
+
   let listTitle = [];
   info.each(function(i, e){
     let a = $(this)
-    // console.log(i + 1);
-    // const desc = e["attribs"]["title"];
     let title = a.text();
     let json = {title}
-    listTitle.push(json)
+    listTitle.push(json);
   });
   console.log(listTitle)
   await browser.close();
